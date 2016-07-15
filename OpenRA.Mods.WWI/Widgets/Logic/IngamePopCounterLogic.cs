527e0bc265d232212e2c359ccf529e1ec969ea99
Christer U.L <int_ch@hotmail.com>
2016-07-15 13:22:06 +0200

    Added pop limit to UI

---
+++ OpenRA.Mods.WWI/Widgets/Logic/IngamePopCounterLogic.cs
@@ -0,0 +1,25 @@
+﻿using OpenRA.Mods.Common.Widgets;
+using OpenRA.Mods.WWI.Traits;
+using OpenRA.Widgets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRA.Mods.WWI.Widgets.Logic
+{
+    public class IngamePopCounterLogic : ChromeLogic
+    {
+        [ObjectCreator.UseCtor]
+        public IngamePopCounterLogic(Widget widget, World world)
+		{
+			var populationManager = world.LocalPlayer.PlayerActor.Trait<PopulationManager>();
+			var label = widget.Get<LabelWithTooltipWidget>("POP");
+            var text = label.Text;
+
+            label.GetText = () => text.F(populationManager.PopulationDrained, populationManager.PopulationDrained + populationManager.ExcessPopulation);
+            label.GetTooltipText = () => "Population: {0}/{1}".F(populationManager.PopulationDrained, populationManager.PopulationDrained + populationManager.ExcessPopulation);
+		}
+    }
+}
