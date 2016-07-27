ae407832d8f5e393b18cc6d686699823ac630d41
Christer U.L <int_ch@hotmail.com>
2016-07-27 15:12:19 +0200

    Shellmap now functional again

--- mods/wwi/maps/empty/shellmap.lua
+++ mods/wwi/maps/empty/shellmap.lua
@@ -2,16 +2,21 @@
 ticks = 99
 speed = 5
 
-FindRandomTarget = function()
-  units = Map.ActorsInBox(Map.TopLeft, Map.BottomRight, function(actor)
-    return actor.Owner == multi0 and actor.HasProperty("Location")
-  end)
+multi0 = null
+multi1 = null
 
-  if table.getn(units) > 0 then
-    return Utils.Random(units)
-  else
-    return null
+GetMulti0 = function()
+  if multi0 == null then
+    multi0 = Player.GetPlayer("Multi0")
   end
+  return multi0
+end
+
+GetMulti1 = function()
+  if multi1 == null then
+    multi1 = Player.GetPlayer("Multi1")
+  end
+  return multi1
 end
 
 FindRandomSpawnLocation = function()
@@ -26,13 +31,13 @@ Tick = function()
 	ticks = ticks + 1
 
 	if ticks % 100 == 0 and ticks < 5000 then
-		local unit0 = Actor.Create("WWI_RIFLE", true, { Owner = multi1, Location = FindRandomSpawnLocation() })
-		local unit1 = Actor.Create("WWI_RIFLE", true, { Owner = multi1, Location = FindRandomSpawnLocation() })
-		local unit2 = Actor.Create("WWI_GRENADIER", true, { Owner = multi1, Location = FindRandomSpawnLocation() })
-		local unit3 = Actor.Create("WWI_RIFLE", true, { Owner = multi1, Location = FindRandomSpawnLocation() })
-		local unit4 = Actor.Create("WWI_RIFLE", true, { Owner = multi1, Location = FindRandomSpawnLocation() })
+		local unit0 = Actor.Create("WWI_RIFLE", true, { Owner = GetMulti1(), Location = FindRandomSpawnLocation() })
+		local unit1 = Actor.Create("WWI_RIFLE", true, { Owner = GetMulti1(), Location = FindRandomSpawnLocation() })
+		local unit2 = Actor.Create("WWI_GRENADIER", true, { Owner = GetMulti1(), Location = FindRandomSpawnLocation() })
+		local unit3 = Actor.Create("WWI_RIFLE", true, { Owner = GetMulti1(), Location = FindRandomSpawnLocation() })
+		local unit4 = Actor.Create("WWI_RIFLE", true, { Owner = GetMulti1(), Location = FindRandomSpawnLocation() })
 
-    local location = FindRandomTarget().location
+    local location = RandomLocationInBase()
 
 		unit0.AttackMove( location )
 		unit1.AttackMove( location )
@@ -43,28 +48,13 @@ Tick = function()
 
   if ticks % 250 == 0 then
     units = Map.ActorsInBox(Map.TopLeft, Map.BottomRight, function(actor)
-			return actor.Owner == multi1 and actor.HasProperty("Location")
+			return actor.Owner == GetMulti1() and actor.HasProperty("Location")
 		end)
 
-    local location = FindRandomTarget().location
+    local location = RandomLocationInBase()
     Utils.Do(units, function(unit)
 			unit.AttackMove( location )
 		end)
   end
-
-end
-
-WorldLoaded = function()
-
-  multi0 = Player.GetPlayer("Multi0")
-  multi1 = Player.GetPlayer("Multi1")
-
-  units = Map.ActorsInBox(Map.TopLeft, Map.BottomRight, function(actor)
-    return actor.Owner == Player.GetPlayer("Multi0")
-  end)
-
-  Utils.Do(units, function(unit)
-    unit.Patrol( {RandomLocationInBase()}, true, 0 )
-  end)
 
 end
