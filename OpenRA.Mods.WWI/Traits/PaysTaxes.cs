612f6258eee350d520109fbc2ba02576ee37e660
Christer U.L <int_ch@hotmail.com>
2016-07-31 12:40:03 +0200

    Improved performace for TaxCollector trait

---
+++ OpenRA.Mods.WWI/Traits/PaysTaxes.cs
@@ -0,0 +1,27 @@
+﻿using OpenRA.Traits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRA.Mods.WWI.Traits
+{
+    public class PaysTaxesInfo : ITraitInfo
+    {
+        [Desc("How much taxes to pay.")]
+        public readonly int Amount = 0;
+
+        public object Create(ActorInitializer init) { return new PaysTaxes(this); }
+    }
+
+    class PaysTaxes
+    {
+        public readonly PaysTaxesInfo info;
+
+        public PaysTaxes(PaysTaxesInfo info)
+		{
+			this.info = info;
+		}
+    }
+}
--- OpenRA.Mods.WWI/Traits/TaxCollector.cs
+++ OpenRA.Mods.WWI/Traits/TaxCollector.cs
@@ -9,33 +9,35 @@ using System.Threading.Tasks;
 
 namespace OpenRA.Mods.WWI.Traits
 {
-    public class TaxCollectionInfo : ITraitInfo
+    public class TaxCollectorInfo : ITraitInfo
     {
         [Desc("Number of ticks to wait between giving money.")]
         public readonly int Period = 375;
+
         [Desc("Amount of money to give as a base value.")]
         public readonly int BaseAmount = 0;
 
         [Desc("Which buildings to collect taxes from.")]
         public readonly string[] Buildings = null;
-        [Desc("How much taxes each buildings pay.")]
-        public readonly int TaxAmount = 0;
 
         [Desc("Maximum credits gained.")]
         public readonly int MaxCredits = Int16.MaxValue;
 
+        [Desc("Max from which to collect from.")]
+        public readonly int CollectionDistance = 0;
+
         [Desc("Whether to show the cash tick indicators (+$15 rising from actor).")]
         public readonly bool ShowTicks = true;
 
-        public object Create(ActorInitializer init) { return new TaxCollection(this); }
+        public object Create(ActorInitializer init) { return new TaxCollector(this); }
     }
 
-    class TaxCollection : ITick
+    class TaxCollector : ITick
     {
-        readonly TaxCollectionInfo info;
+        readonly TaxCollectorInfo info;
         [Sync] int ticks;
 
-        public TaxCollection(TaxCollectionInfo info)
+        public TaxCollector(TaxCollectorInfo info)
 		{
 			this.info = info;
 		}
@@ -44,14 +46,17 @@ namespace OpenRA.Mods.WWI.Traits
         {
             if (--ticks < 0)
             {
-                var buildings = (
-                    from a in self.World.ActorsWithTrait<Building>()
-                    where a.Actor.Owner == self.Owner && info.Buildings.Contains(a.Actor.Info.Name)
-                    select a);
-
                 ticks = info.Period;
 
-                int amount = Math.Min(info.BaseAmount + info.TaxAmount * buildings.Count(), info.MaxCredits);
+                int amount = 0;
+
+                amount += (
+                    from a in self.World.ActorsWithTrait<PaysTaxes>()
+                    where a.Actor.Owner == self.Owner && info.Buildings.Contains(a.Actor.Info.Name) && (info.CollectionDistance == 0 || info.CollectionDistance > (a.Actor.CenterPosition - self.CenterPosition).HorizontalLength )
+                    select a.Trait.info.Amount).Sum();
+
+                amount += info.BaseAmount;
+                amount = Math.Min(amount, info.MaxCredits);
 
                 self.Owner.PlayerActor.Trait<PlayerResources>().GiveCash(amount);
 
